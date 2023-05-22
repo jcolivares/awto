2847afecde3b70b9f42316339044428f8817b95b
Juan Carlos Olivares R <jcolivares@itmorelia.edu.mx>
2023-05-22 11:53:34 -0600

    agregando conexion a consulta bd

--- login.php
+++ login.php
@@ -1,8 +1,9 @@
 <?php
-$usuario = $_POST['usuario'];
-$contraseña = $_POST['contraseña'];
+//$_REQUEST encapsula $_GET $_POST
+$usuario = $_REQUEST['usuario'];
+$contraseña = $_REQUEST['contraseña'];
 
-include("conexion.php");
+require("conexion.php");
 
 ?>
 <!DOCTYPE html>
@@ -14,21 +15,91 @@ include("conexion.php");
     <title>Login</title>
 </head>
 <body>
-    <h1><?=$usuario?></h1>
+    <h1>Bienvenido <?=$usuario?></h1>
     <?php
     //echo "<h1>$contraseña</h1>";
 
     $conn = new mysqli($host, $user, $passwd, $db);
 
     if($conn->connect_error){
-        die("Error al conectar la base datos:");
+        die("Error al conectar la base datos:".$conn->connect_error);
     }else{
         ?>
 
     <p>Conectado con exito!!!</p>
 
         <?php
+$sql = "SELECT 
+            pacientes.id as id,
+            pacientes.nombre,
+            ap_paterno,
+            ap_materno,
+            ocupacion.nombre as descripcion,
+            sexo,
+            usuario,
+            contraseña 
+        FROM 
+            pacientes, ocupacion
+        WHERE
+            pacientes.ocupacion = ocupacion.id
+        ";
+
+$resultado = $conn->query($sql);
+$usuarios = $resultado->num_rows;
+
+    if($usuarios>0){ 
+?>
+    <p>Hay <?=$usuarios;?> usuarios</p>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Apellido Paterno</th>
+                <th>Apellido Materno</th>
+                <th>Ocupacion</th>
+                <th>Sexo</th>
+                <th>Usuario</th>
+                <th>Password</th>
+            </tr>
+        </thead>
+        <tbody>
+        
+        <?php
+            while($fila=$resultado->fetch_assoc()){
+
+        ?>
+            <tr>
+                <td><?=$fila['id'];?></td>
+                <td><?=$fila['nombre'];?></td>
+                <td><?=$fila['ap_paterno'];?></td>
+                <td><?=$fila['ap_materno'];?></td>
+                <td><?=$fila['descripcion'];?></td>
+                <td><?=$fila['sexo'];?></td>
+                <td><?=$fila['usuario'];?></td>
+                <td><?=$fila['contraseña'];?></td>
+            </tr>  
+            
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>    
+
+<?php
+        
+        }
+    else {
+?>
+
+<h1>Sin usuarios</h1>
+
+<?php
     }
+}
+    //cerrar conexion
+    $conn->close();
+
     ?>
 </body>
 </html>
